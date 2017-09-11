f3f0dc4cedb95e4a28bbbf7a633538d94191b8d2
Omkar Vyas <ovyas@qti.qualcomm.com>
2017-09-11 10:55:20 -0700

    C++ first project with basics tutorial

---
+++ C++/Basic.cpp
@@ -0,0 +1,18 @@
+/*
+ * Basic.cpp
+ *
+ *  Created on: Sep 11, 2017
+ *      Author: ovyas
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << "Hello CPP\n";
+	return 0;
+
+}
+
+
