b2f6027df84ee60fe8b67cfd0de38c0a452ccc83
Omkar Vyas <ovyas@qti.qualcomm.com>
2017-09-12 16:59:50 -0700

    shifted to LearnCpp.com for better theory reached till http://www.learncpp.com/cpp-tutorial/14c-keywords-and-naming-identifiers/ location

--- C++/Tutorials/T1.cpp
+++ C++/Tutorials/T1.cpp
@@ -5,10 +5,43 @@
  *      Author: ovyas
  */
 
-
+/*
+ * Common problems http://www.learncpp.com/cpp-tutorial/07-a-few-common-cpp-problems/
+ *
+ * */
 
 #include <iostream>
 using namespace std;
+
+void doPrint(){
+
+	cout << "in function doPrint() " <<endl;
+
+}
+
+int return5(){
+
+	return 5;
+}
+
+int add(int x, int y){
+
+	return x+y;
+
+}
+
+
+int multiply(int z, int w){
+
+	return z*w;
+
+}
+
+int doubleNumber(int x){
+
+	return 2*x;
+
+}
 
 int main(){
 
@@ -32,8 +65,28 @@ int main(){
 
 	d = blue;
 	cout << "d: " << d << endl;
+	/*
+	int in_v = 0;
+	cout << "Input some value" <<endl;
+	cin >> in_v;
+	cout << "you input this value: " << in_v <<endl;
+	*/
+    doPrint();
 
+    //cout << doPrint() << endl; /*doPrint is void returning fuction so such use is illigal*/
+    cout << return5 <<endl; //This program will compile, but the function will not be called because the function call is missing parenthesis.
+                            //answer depends on compiler, in this case it just returns 1
 
+    cout << "addition of 3 and 5: " << add(3,5) << endl;
+    cout << "multiplication of 3 and 5: " << multiply(3,5) << endl;
+
+    cout << "add(add(3,5),2): " << add(add(3,5),2) <<endl;
+    cout << "multiply(add(3,5),2): " << multiply(add(3,5),2) <<endl;
+    cout << "Double the number 10: " << doubleNumber(10);
+    cout << "input number:";
+    int in_val = 0;
+    cin >> in_val;
+    cout << "Double of your number = " << doubleNumber(in_val) << endl;
 	return 0;
 
 
